f0ab0412d4bff9177f41ccc94f638bdd4783e85c
AnnScherer <ann.hardt@gmx.de>
2023-04-07 11:52:48 +0200

    change cors

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://dnd-frontend-seven.vercel.app",
     credentials: true,
   })
 );
